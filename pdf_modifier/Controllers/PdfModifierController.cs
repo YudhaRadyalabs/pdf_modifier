372a84250a994e7d5ac57013e5a3c11ffb320dad
Yudha Satria <yudha.satria.0890@gmail.com>
2024-10-11 14:23:45 +0700

    using iText7

--- pdf_modifier/Controllers/PdfModifierController.cs
+++ pdf_modifier/Controllers/PdfModifierController.cs
@@ -1,7 +1,11 @@
+using iText.Kernel.Pdf.Canvas.Parser;
 using Microsoft.AspNetCore.Mvc;
-using PdfSharp.Pdf;
-using PdfSharp.Pdf.IO;
 using System.IO.Compression;
+
+//using PdfSharp.Pdf;
+//using PdfSharp.Pdf.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
 
 namespace pdf_modifier.Controllers
 {
@@ -22,30 +26,89 @@ namespace pdf_modifier.Controllers
             var zipStream = new MemoryStream();
             try
             {
+
                 using (ZipArchive zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Create, true))
                 using (var stream = new MemoryStream())
                 {
                     file.CopyTo(stream);
                     stream.Position = 0;
 
-                    PdfDocument inputPdf = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
-
-                    for (int pageIndex = 0; pageIndex < inputPdf.PageCount; pageIndex++)
+                    using (PdfReader reader = new PdfReader(stream))
+                    using (PdfDocument pdfDoc = new PdfDocument(reader))
                     {
-                        PdfDocument outputPdf = new PdfDocument();
-                        outputPdf.Version = inputPdf.Version;
-                        outputPdf.Info.Title = $"Page {pageIndex + 1} of {inputPdf.Info.Title}";
-                        outputPdf.Info.Creator = inputPdf.Info.Creator;
-
-                        outputPdf.AddPage(inputPdf.Pages[pageIndex]);
-
-                        var zipEntry = zipArchive.CreateEntry($"file.pdf", CompressionLevel.Fastest);
-                        using (var entryStream = zipEntry.Open())
+                        for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
                         {
-                            outputPdf.Save(entryStream, closeStream: false);
+                            string pageText = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(i));
+                            var splitPageText = pageText.Split("\n");
+                            //find page
+                            var pages = splitPageText.Where(x => x.Contains("dari")).ToList();
+                            int strPage = 0;
+                            int endpage = 0;
+                            foreach (var page in pages)
+                            {
+                                var numberPage = page.Split("dari");
+                                //start page
+                                if (int.TryParse(numberPage[0].Trim(), out int resultStr))
+                                {
+                                    strPage = i;
+                                }
+                                //end page
+                                if (int.TryParse(numberPage[1].Trim(), out int resultEnd))
+                                {
+                                    endpage = i + resultEnd - 1;
+                                }
+                            }
+                            if (strPage > 0 && endpage > 0)
+                            {
+                                string noFaktur = splitPageText[2].Replace("Kode dan Nomor Seri Faktur Pajak :", "");
+                                var zipEntry = zipArchive.CreateEntry($"{noFaktur}.pdf");
+                                using (var entryStream = zipEntry.Open())
+                                {
+                                    using (PdfWriter pdfWriter = new PdfWriter(entryStream))
+                                    using (PdfDocument splitPdfDoc = new PdfDocument(pdfWriter))
+                                    {
+                                        // Create a Document object
+                                        Document document = new Document(splitPdfDoc);
+                                        pdfDoc.CopyPagesTo(strPage, endpage, splitPdfDoc);
+                                        document.Close();
+                                        i = endpage;
+                                    }
+                                }
+                            }
                         }
                     }
                 }
+
+                //using (ZipArchive zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Create, true))
+                //using (var stream = new MemoryStream())
+                //{
+                //    file.CopyTo(stream);
+                //    stream.Position = 0;
+
+                //    PdfDocument inputPdf = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
+
+                //    foreach (var page in inputPdf.Pages)
+                //    {
+                //        string pageText = PdfTextExtractor.GetTextFromPage(page);
+                //    }
+
+                //    for (int pageIndex = 0; pageIndex < inputPdf.PageCount; pageIndex++)
+                //    {
+                //        PdfDocument outputPdf = new PdfDocument();
+
+                //        outputPdf.Version = inputPdf.Version;
+                //        outputPdf.Info.Title = $"Page {pageIndex + 1} of {inputPdf.Info.Title}";
+                //        outputPdf.Info.Creator = inputPdf.Info.Creator;
+
+                //        outputPdf.AddPage(inputPdf.Pages[pageIndex]);
+
+                //        var zipEntry = zipArchive.CreateEntry($"file.pdf", CompressionLevel.Fastest);
+                //        using (var entryStream = zipEntry.Open())
+                //        {
+                //            outputPdf.Save(entryStream, closeStream: false);
+                //        }
+                //    }
+                //}
             }
             catch (Exception ex)
             {
